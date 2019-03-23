e2e2ec49f75323226e0c1e44c11e636301232ac7
zdoh <denfairycat@gmail.com>
2019-03-23 15:51:43 +0300

    Initial commit

---
+++ src/main/java/ru/zdoher/library/OtusLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.zdoher.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusLibraryApplication.class, args);
+    }
+
+}
