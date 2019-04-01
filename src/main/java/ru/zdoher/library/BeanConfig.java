77a10bea892df3c020e279c6ecfbc973051be203
zdoh <denfairycat@gmail.com>
2019-04-01 17:35:42 +0300

    add jdbc bean

---
+++ src/main/java/ru/zdoher/library/BeanConfig.java
@@ -0,0 +1,21 @@
+package ru.zdoher.library;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+
+@Service
+public class BeanConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/library");
+        ds.setUsername("libraryUser");
+        ds.setPassword("libraryPass");
+        return ds;
+    }
+}
--- src/main/java/ru/zdoher/library/OtusLibraryApplication.java
+++ src/main/java/ru/zdoher/library/OtusLibraryApplication.java
@@ -2,8 +2,10 @@ package ru.zdoher.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class OtusLibraryApplication {
 
     public static void main(String[] args) {
